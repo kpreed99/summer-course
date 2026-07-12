7983aa1a4bb85cb8e839dcbfa3fc27f9f6ad7f14
kpreed99 <kyle.p.reed1986@gmail.com>
2026-07-12 17:28:43 -0400

    Saved Changes

--- Python/Lesson 01 - Python Basics/lesson1_notes.py
+++ Python/Lesson 01 - Python Basics/lesson1_notes.py
@@ -1,1 +1,12 @@
+#Lesson 1 Python
 
+name = input("what is your name? ")
+pizza_dia = input("what is the diameter of the pizza? ")
+dia_float = float(pizza_dia)
+pizza_area = 3.14 * (dia_float / 2) **2
+
+
+
+
+print("Hello", (name))
+print("The area is " + str(pizza_area))

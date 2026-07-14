def2785457e557dfd067c7d7b1a1f44b8f50f282
kpreed99 <kyle.p.reed1986@gmail.com>
2026-07-14 16:52:13 -0400

    update

--- Python/Lesson 02 - Conditionals and Control Structures/lesson2_notes.py
+++ Python/Lesson 02 - Conditionals and Control Structures/lesson2_notes.py
@@ -45,8 +45,32 @@
 # else:
 #     print("F")
 
-number = int(input("Enter an even number "))
-while number %2 != 0:
-    print("This is an odd number")
-    number = int(input("Enter and even number "))
-print ("Good Job, That is an even number!")
+# number = int(input("Enter an even number "))
+# while number %2 != 0:
+#     print("This is an odd number")
+#     number = int(input("Enter and even number "))
+# print ("Good Job, That is an even number!")
+
+# user_name = input("What is your name? ")
+# fav_num = input("What is your favorite number? ") 
+
+# print("`" * 3)
+# print("*" * 30)
+# greeting = 'Hello, ' + user_name.strip().title() + "!"
+# white_space_length = (30 - len(greeting) - 11)
+# print("* " + greeting + "  " * white_space_length + "*")
+# fav_num = 'Your favorite number is ' + fav_num 
+# print('* ' + fav_num + ' *')
+# print("*" * 30)
+# print("`" * 3)
+
+# for number in range(1,16):
+#     print(number, end=" ")
+
+
+# for number in range(2,31):
+#     if(number % 2 == 0):
+#         print(number, end=" ")
+
+# for number in range(20,-1,-2):
+#     print(number, end=" ")

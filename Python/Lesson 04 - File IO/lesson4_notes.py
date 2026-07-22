4397410666e41947ca978445e2a12f35bef60724
kpreed99 <kyle.p.reed1986@gmail.com>
2026-07-22 11:16:05 -0400

    update

---
+++ Python/Lesson 04 - File IO/lesson4_notes.py
@@ -0,0 +1,42 @@
+# import random
+
+# game_list  = [1, 3, 5, 7, 9]
+# total_games = int(input("How many games would you like to play (1-9), odd number only: "))
+# game_choice_list = ["rock", "paper", "scissor"]
+# number_of_games = input("")
+# user_prompt = input("Please pick rock, paper, or scissor: ")
+
+# while total_games not in game_list:
+#     print("Please enter an odd number between 1 and 9")
+#     total_games = int(input("How many games would you like to play (1-9), odd number only: "))
+
+# user_wins = 0
+# cpu_wins = 0
+# wins_needed = (total_games // + 2) + 1
+
+# print(total_games)
+# print(wins_needed)
+
+# computer_selection = random.choice(game_list)
+# print(computer_selection)
+
+# if user_prompt == "rock" and computer_selection == "paper":
+#     print("You lose...")
+# elif user_prompt == "paper" and computer_selection == "rock":
+#     print("You win!")
+# elif user_prompt == "scissor" and computer_selection == "paper":
+#     print("You win!")
+# elif user_prompt == "rock" and computer_selection == "scissor":
+#     print("You win")
+# elif user_prompt == "paper" and computer_selection == "scissor":
+#     print("You lose...")
+# elif user_prompt == "scissor" and computer_selection == "rock":
+#     print("You lose...")
+# else:
+#     print("Tie!")
+
+
+# def compound_interest(principal, interest, n: int = 1, t: int):
+#     return principal * (1 + interest/number_of_times_compound) ** (n * t)
+
+

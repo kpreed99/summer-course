e964984c160b023b4645d454b5b9bb04a2444c2b
kpreed99 <kyle.p.reed1986@gmail.com>
2026-07-27 14:22:29 -0400

    update

--- Python/Lesson 07 - Data Structures/lesson7_notes.py
+++ Python/Lesson 07 - Data Structures/lesson7_notes.py
@@ -1,12 +1,18 @@
 
-coordinates = []
-with open("preclass_problem1_data.txt", 'r') as file:
+# coordinates = []
+# with open("preclass_problem1_data.txt", 'r') as file:
    
-    for line in file:
-        coordinate = int(line)
-        coordinates.append(int(line.strip()))
+#     for line in file:
+#         coordinate = int(line)
+#         coordinates.append(int(line.strip()))
 
-highest = max(range(coordinates), 5)
-grid = sum(highest) / len(highest)
-print(highest)
-print(grid)
+# highest = max(range(coordinates), 5)
+# grid = sum(highest) / len(highest)
+# print(highest)
+# print(grid)
+unit = {}
+unit["Reed"] = {"rank": "PFC", "years_of_service": 2}
+unit["Bonnie"] = {"rank": "SFC", "years_of_service": 10}
+unit["Reeves"] = {"rank": "PFC", "years_of_service": 2}
+unit["Red"] = {"rank": "SPC", "years_of_service": 3}
+unit["Morsey"] = {"rank": "SGT", "years_of_service": 5}

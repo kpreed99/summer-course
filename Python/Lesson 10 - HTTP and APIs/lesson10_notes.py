0f2517e37cc63633522001fe3c1544d2b93b740a
kpreed99 <kyle.p.reed1986@gmail.com>
2026-08-03 14:45:55 -0400

    update

--- Python/Lesson 10 - HTTP and APIs/lesson10_notes.py
+++ Python/Lesson 10 - HTTP and APIs/lesson10_notes.py
@@ -1,9 +1,18 @@
 import requests
-url = "https://jsonplaceholder.typicode.com/posts/1"
-response = requests.get(url)
 
-print(response.text)
-print(response.headers["content-type"])
-print(response.elapsed)
-print(response.reason)
+# url = "https://jsonplaceholder.typicode.com/posts/1"
+# response = requests.get(url)
+
+# print(response.text)
+# print(response.headers["content-type"])
+# print(response.elapsed)
+# print(response.reason)
+# print(response.status_code)
+
+BASE_URL = "https://httpbin.org"
+
+headers = {"Authorization": "Bearer abc123"}
+response = requests.get(f"{BASE_URL}/bearer", headers=headers)
+
 print(response.status_code)
+print(response.json())

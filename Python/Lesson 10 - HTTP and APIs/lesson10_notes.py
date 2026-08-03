dd4bfdf3b0c579cc04ab623f22ab2bddfad92030
kpreed99 <kyle.p.reed1986@gmail.com>
2026-08-03 11:53:37 -0400

    update

---
+++ Python/Lesson 10 - HTTP and APIs/lesson10_notes.py
@@ -0,0 +1,9 @@
+import requests
+url = "https://jsonplaceholder.typicode.com/posts/1"
+response = requests.get(url)
+
+print(response.text)
+print(response.headers["content-type"])
+print(response.elapsed)
+print(response.reason)
+print(response.status_code)

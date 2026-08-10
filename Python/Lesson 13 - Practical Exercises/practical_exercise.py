6daf5a08e13b0e8773bff56549b494840282b59f
kpreed99 <kyle.p.reed1986@gmail.com>
2026-08-10 11:44:43 -0400

    update

--- Python/Lesson 13 - Practical Exercises/practical_exercise.py
+++ Python/Lesson 13 - Practical Exercises/practical_exercise.py
@@ -1,6 +1,4 @@
 metric_type = input(f"Do you want Miles above Mars or Kilometers above Mars?: ")
-# miles_mars = int(input("Enter number of miles: "))
-# kilometers_mars = int(input("Enter number of kilometers: "))
 
 if metric_type == "miles above mars":
     miles_mars = int(input("Enter number of miles: "))

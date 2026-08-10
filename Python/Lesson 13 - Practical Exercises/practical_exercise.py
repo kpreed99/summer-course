73daf2880ed3193dae2fac6383e11c9dccb6017b
kpreed99 <kyle.p.reed1986@gmail.com>
2026-08-10 12:49:11 -0400

    update

--- Python/Lesson 13 - Practical Exercises/practical_exercise.py
+++ Python/Lesson 13 - Practical Exercises/practical_exercise.py
@@ -1,22 +1,41 @@
-metric_type = input(f"Do you want Miles above Mars or Kilometers above Mars?: ")
+# metric_type = input(f"Do you want Miles above Mars or Kilometers above Mars?: ")
 
-if metric_type == "miles above mars":
-    miles_mars = int(input("Enter number of miles: "))
-    yards = miles_mars * 1760
-    feet = miles_mars * 5280
-    inches = miles_mars * 63360
-    print(f"There are {yards}yds in {miles_mars} miles")
-    print(f"There are {feet}ft in {miles_mars} miles")
-    print(f"There are {inches}in in {miles_mars} miles")
-elif metric_type == "kilometers above mars":
-    kilometers_mars = int(input("Enter number of kilometers: "))
-    meters = kilometers_mars * 1000
-    centimeters = kilometers_mars * 100000
-    millimeters = kilometers_mars * 1000000
-    print(f"There are {meters}yds in {kilometers_mars} kilometers")
-    print(f"There are {centimeters}yds in {kilometers_mars} kilometers")
-    print(f"There are {millimeters}yds in {kilometers_mars} kilometers")
-else:
-    metric_type = input("Do you want Miles above Mars or Kilometers above Mars?: ")
+# if metric_type == "miles above mars":
+#     miles_mars = int(input("Enter number of miles: "))
+#     yards = miles_mars * 1760
+#     feet = miles_mars * 5280
+#     inches = miles_mars * 63360
+#     print(f"There are {yards}yds in {miles_mars} miles")
+#     print(f"There are {feet}ft in {miles_mars} miles")
+#     print(f"There are {inches}in in {miles_mars} miles")
+# elif metric_type == "kilometers above mars":
+#     kilometers_mars = int(input("Enter number of kilometers: "))
+#     meters = kilometers_mars * 1000
+#     centimeters = kilometers_mars * 100000
+#     millimeters = kilometers_mars * 1000000
+#     print(f"There are {meters}yds in {kilometers_mars} kilometers")
+#     print(f"There are {centimeters}yds in {kilometers_mars} kilometers")
+#     print(f"There are {millimeters}yds in {kilometers_mars} kilometers")
+# else:
+#     metric_type = input("Do you want Miles above Mars or Kilometers above Mars?: ")
 
+
+
+# def area_circle(radius):
+#     return(3.14 * (radius)**2)
+# def area_triangle(base, height):
+#     return(0.5 * base * height)
+# def area_square(length, width):
+#     return(length * width)
+
+# circular_pizzas = area_circle(7.5)*2
+# triangle_pizzas = area_triangle(20, 20)
+# square_pizza = area_square(18, 18)
+
+# if circular_pizzas > triangle_pizzas and square_pizza:
+#     print("The circular pizzas are the best deal")
+# elif triangle_pizzas > circular_pizzas and square_pizza:
+#     print("The triangle pizza is the best deal")
+# else:
+#     print("The square pizza is the best deal")
 
